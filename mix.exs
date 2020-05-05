448c9776e827b8c0b2425a3659cf6098ea4e76b5
khanhdoth <khanh.doth@gmail.com>
2020-05-05 21:22:14 +0000

    u

--- mix.exs
+++ mix.exs
@@ -4,8 +4,15 @@ defmodule Tanx.Umbrella.Mixfile do
   def project do
     [
       apps_path: "apps",
+      version: "0.1.0",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      releases: [
+        tanx: [
+          include_executables_for: [:unix],
+          applications: [runtime_tools: :permanent]
+        ]
+      ]
     ]
   end
 
